3095ed96af4fbd283a53b26d3152b6ab0eb22506
Prince-Ugwa <odiniyaprinceugwa@gmail.com>
2022-08-05 21:14:27 +0100

    commit

--- js/style.js
+++ js/style.js
@@ -1,5 +1,11 @@
 const crud = document.querySelector(".crud");
+const crudClose = document.querySelector(".crud-close");
+const crudOpen = document.querySelector(".crud-open");
 
-crud.addEventListener("click", () => {
-  crud.classList.
+crudOpen.addEventListener("click", function () {
+  crud.classList.remove("hidden");
+});
+
+crudClose.addEventListener("click", function () {
+  crud.classList.add("hidden");
 });
